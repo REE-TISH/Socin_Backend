fab43176ba15766cf5efe00b12cb0bc8034c544d
REE-TISH <reetishk798@gmail.com>
2026-02-23 18:02:50 +0530

    Update database settings for production environment

--- Socin_backend/Socin_backend/settings.py
+++ Socin_backend/Socin_backend/settings.py
@@ -115,19 +115,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # Database for development
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',   
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',   
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Database for Production
-# DATABASES = {
-#     'default':dj_database_url.config(
-#         default=config('DATABASE_URL') 
-#     )
-# }
+DATABASES = {
+    'default':dj_database_url.config(
+        default=config('DATABASE_URL') 
+    )
+}
 
 
 CACHES = {
